bd227d6748812639c7ddec32bbfca934c7a92513
Nzeako, Kosisochukwu Anthony <nzeakokosi7@gmail.com>
2022-09-04 21:40:01 +0100

    Update SpawnManagerX.cs
    fixed bug with ball prefab rotation

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -27,7 +27,7 @@ public class SpawnManagerX : MonoBehaviour
 
         // instantiate ball at random spawn location
         int ballIndex = Random.Range(0, ballPrefabs.Length);
-        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
